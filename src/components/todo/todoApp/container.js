bae59bb563a7b8868c9274f2b4eeae8ed7c6e708
Tony Park <tonypark0403@gmail.com>
2019-01-12 15:28:04 -0500

    added input field

--- src/components/todo/todoApp/container.js
+++ src/components/todo/todoApp/container.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 import TodoTemplate from '../todoTemplate';
+import TodoInput from '../TodoInput';
 
 class container extends Component {
+
+    state = {
+        input: '',
+        todos: [
+            { id: 0, text: 'Study React', done: true },
+            { id: 1, text: 'Apply Style-component', done: false }
+        ]
+    }
+
+    id = 1
+    
+    getId = () => {
+        return ++this.id;
+    }
+
+    handleChange = (e) => {
+        const { value } = e.target;
+        this.setState({
+            input: value
+        });
+    }
+
+    handleInsert = () => {
+    }
+
     render() {
+        const {
+            input
+        } = this.state;
+        const {
+            handleChange,
+            handleInsert
+        } = this;
         return (
-            <TodoTemplate>Welcome to this App!</TodoTemplate>
+            <TodoTemplate>
+                <TodoInput onChange={handleChange} onInsert={handleInsert} value={input} />
+            </TodoTemplate>
         );
     }
 }
